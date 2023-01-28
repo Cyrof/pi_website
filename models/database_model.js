9ae566f8f0b815153efc2a13c3a7e660fe91c169
Keith <keithneo00@gmail.com>
2023-01-29 03:53:43 +0800

    create signup page

---
+++ models/database_model.js
@@ -0,0 +1,32 @@
+// function to create database model 
+
+const mongoose = require('mongoose');
+
+const dataschema = new mongoose.Schema({
+    fname: {
+        required: true,
+        type: String
+    },
+    lname: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    uname: {
+        required: true,
+        type: String
+    },
+    pwd: {
+        required: true,
+        type: String
+    },
+    access: {
+        required: true,
+        type: String
+    }
+});
+
+module.exports = mongoose.model('User', dataschema);
--- routes/sign_up.js
+++ routes/sign_up.js
@@ -2,9 +2,23 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/database_model');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     res.render('../views/sign_up');
+});
+
+router.post('/', async (req, res) => {
+    res.render('../views/sign_up');
+    const body = req.body;
+    body.access = "denied";
+    // create mongoose instance for new user
+    const user = new User(body);
+    // gen salt than hash password
+    const salt = await bcrypt.genSalt(10);
+    user.pwd = await bcrypt.hash(user.pwd, salt);
+    user.save();
 });
 
 module.exports = router;
