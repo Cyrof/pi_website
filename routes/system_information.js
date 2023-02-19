b4214b6c65e71a1dc0138a4e1ea501f41b3460a2
Keith <keithneo00@gmail.com>
2023-02-19 18:07:26 +0800

    update sys info, home and side bar

--- routes/system_information.js
+++ routes/system_information.js
@@ -3,17 +3,18 @@
 const express = require('express');
 const router = express.Router();
 const checker = require('../scripts/sess_checker')
+const User = require('../models/database_model');
 
 
 router.get('/', checker, async (req, res) => {
-    // res.render('../views/login', {valid: false, error: ""});
-    let data = require('../scripts/system_info');
-    // data = await data.systemInfo();
-    // console.log(data);
-    data.systemInfo().then(d => {
-        res.render('../views/system_info', {sys_info: d});
-    });
-    // res.render('../views/system_info', {sys_info: data});
+    let data = await require('../scripts/system_info').systemInfo();
+    let token = req.session.token;
+    let user = await User.find({token: token});
+    user = user[0]
+    // data.systemInfo().then(d => {
+    //     res.render('../views/system_info', {sys_info: d});
+    // });
+    res.render('../views/system_info', {sys_info: data, page: 'System Information', user: user.uname})
 })
 
 module.exports = router;
