e5c5541b60b96606a05f1d4fc77ec470e92fdd7d
Keith <keithneo00@gmail.com>
2023-03-08 14:51:51 +0800

    update file types

--- scripts/folderContent.js
+++ scripts/folderContent.js
@@ -11,12 +11,16 @@ const directoryPath = process.env.SHARED_FOLDER_PATH
 function getContentDetails(p=directoryPath){
     let details = {}
     // let newdetails = {}
+    console.log(p)
     let files = fs.readdirSync(p)
     let objNum = 1
     files.forEach((element) => {
         stats = fs.statSync(path.join(p, element));
         let id = Buffer.from(element, 'utf-8').toString('hex');
-        let type = mime.lookup(element) !== false ? mime.lookup(element) : 'directory';
+        let check_path_directory = stats.isDirectory() ? true : false;
+        // let type = mime.lookup(element) !== false ? mime.lookup(element) : 'directory';
+        let type = check_path_directory ? 'directory' : mime.lookup(element);
+        type = type === false ? 'unknown' : type;
         let size = stats.size;
         let modified = stats.mtime;
         let f = {name: element, type: type, size: size, modified: modified};
