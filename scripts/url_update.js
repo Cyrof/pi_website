fc42371f3dc33543a3a155bc53798a268c3dfbfd
keith <keithneo00@gmail.com>
2023-04-22 02:28:57 +0800

    update exit code @ url_update.js

--- scripts/url_update.js
+++ scripts/url_update.js
@@ -16,31 +16,36 @@ const branch = process.env.GIT_BRANCH
 // function to push updated readme file to github 
 
 async function pushReadme() {
-    await git.addConfig('user.name', process.env.GIT_NAME, append=true, scope="global")
-    await git.addConfig('user.email', process.env.GIT_EMAIL, append=true, scope="global")
-    await git.addRemote('user', `https://${process.env.GIT_UNAME}:${process.env.GIT_PAT}@github.com/${process.env.GIT_UNAME}/pi_website.git`)
-    await git.checkout(branch)
-    await git.add(path);
-    console.log("README.md staged...")
-    await git.commit('Update readme');
-    console.log("Committed changes...")
-    await git.push('user', branch);
-    console.log("Pushed to github...")
+    try {
+        await git.addConfig('user.name', process.env.GIT_NAME, append = true, scope = "global")
+        await git.addConfig('user.email', process.env.GIT_EMAIL, append = true, scope = "global")
+        await git.addRemote('user', `https://${process.env.GIT_UNAME}:${process.env.GIT_PAT}@github.com/${process.env.GIT_UNAME}/pi_website.git`)
+        await git.checkout(branch)
+        await git.add(path);
+        console.log("README.md staged...")
+        await git.commit('Update readme');
+        console.log("Committed changes...")
+        await git.push('user', branch);
+        console.log("Pushed to github...")
+    } catch (err) {
+        console.error(err);
+        process.exit(0);
+    }
 }
 
 // ========================================= //
 // function to get content of the readme file and update the url
 
-var update_url = async function(url) {
+var update_url = async function (url) {
     await git.pull('origin', branch)
     console.log("Pull from github...")
-    fs.readFile(path, function(err, data){
+    fs.readFile(path, function (err, data) {
         if (err) throw err;
 
         var content = data.toString().split('\n')
 
         content.forEach((line, i) => {
-            if (line.includes("URL")){
+            if (line.includes("URL")) {
                 var new_url = content[i].split(' ');
                 new_url.pop()
                 new_url.push(url)
